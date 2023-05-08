41e633447d924280dc0db65a6ded388c8cca10c9
Oleh Kaliuzhnyi <el_commandor@outlook.com>
2023-05-08 13:16:10 +0300

    feat: add firebase service

---
+++ src/servises/firebase.ts
@@ -0,0 +1,72 @@
+import { initializeApp } from 'firebase/app';
+import {
+  getAuth,
+  signInWithEmailAndPassword,
+  createUserWithEmailAndPassword,
+  sendPasswordResetEmail,
+  signOut,
+} from 'firebase/auth';
+import { getFirestore, collection, addDoc } from 'firebase/firestore';
+
+const firebaseConfig = {
+  apiKey: import.meta.env.VITE_FB_API_KEY,
+  authDomain: 'react-rs-graphiql.firebaseapp.com',
+  projectId: 'react-rs-graphiql',
+  storageBucket: 'react-rs-graphiql.appspot.com',
+  messagingSenderId: import.meta.env.VITE_FB_SENDER_ID,
+  appId: import.meta.env.VITE_FB_APP_ID,
+};
+
+const app = initializeApp(firebaseConfig);
+const auth = getAuth(app);
+const db = getFirestore(app);
+
+const logInWithEmailAndPassword = async (email: string, password: string) => {
+  try {
+    await signInWithEmailAndPassword(auth, email, password);
+  } catch (err) {
+    if (err instanceof Error) {
+      console.error(err);
+      alert(err.message);
+    }
+  }
+};
+const registerWithEmailAndPassword = async (name: string, email: string, password: string) => {
+  try {
+    const res = await createUserWithEmailAndPassword(auth, email, password);
+    const user = res.user;
+    await addDoc(collection(db, 'users'), {
+      uid: user.uid,
+      name,
+      authProvider: 'local',
+      email,
+    });
+  } catch (err) {
+    if (err instanceof Error) {
+      console.error(err);
+      alert(err.message);
+    }
+  }
+};
+const sendPasswordReset = async (email: string) => {
+  try {
+    await sendPasswordResetEmail(auth, email);
+    alert('Password reset link sent!');
+  } catch (err) {
+    if (err instanceof Error) {
+      console.error(err);
+      alert(err.message);
+    }
+  }
+};
+const logout = () => {
+  signOut(auth);
+};
+export {
+  auth,
+  db,
+  logInWithEmailAndPassword,
+  registerWithEmailAndPassword,
+  sendPasswordReset,
+  logout,
+};
