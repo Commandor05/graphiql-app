af1253120be1340acb06dcfd604d4ec2066763a5
Viktor Solovyev <barn-mih@rambler.ru>
2023-05-09 21:23:09 +0300

    add i18n-react

--- src/App.tsx
+++ src/App.tsx
@@ -1,7 +1,16 @@
+import { I18nextProvider, useTranslation } from 'react-i18next';
+import i18n from './shared/locales/i18next';
+import SelectLanguage from './features/SelectLanguage';
+
 function App() {
+  const { t } = useTranslation();
+
   return (
     <>
-      <h1> React. GraphiQL</h1>
+      <I18nextProvider i18n={i18n}>
+        <h1>{t('hello')} React. GraphiQL</h1>
+        <SelectLanguage></SelectLanguage>
+      </I18nextProvider>
     </>
   );
 }
