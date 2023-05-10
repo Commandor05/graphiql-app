4d93c236024c9c8a3a69a6d52a88aaf58242ca2f
Oleh Kaliuzhnyi <el_commandor@outlook.com>
2023-05-10 20:03:39 +0300

    feat: add store with user state

---
+++ src/redux/features/user/userSlice.ts
@@ -0,0 +1,24 @@
+import { createSlice, PayloadAction } from '@reduxjs/toolkit';
+import { User } from '../../../types/User';
+
+type UserState = User;
+
+const initialState: UserState = {
+  isAuthenticated: false,
+};
+
+const userSlice = createSlice({
+  name: 'user',
+  initialState,
+  reducers: {
+    authenticatedUser(state, action: PayloadAction<User>) {
+      return (state = { ...action.payload, isAuthenticated: true });
+    },
+    logedOutUser() {
+      return { isAuthenticated: false };
+    },
+  },
+});
+
+export const { authenticatedUser, logedOutUser } = userSlice.actions;
+export default userSlice.reducer;
