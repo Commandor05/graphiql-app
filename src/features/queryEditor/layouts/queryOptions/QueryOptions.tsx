e026aa3607853594c90cb30851dc6922571811bf
Szyrwel <potato_father@list.ru>
2023-05-23 22:30:14 +0300

    fix: fix style

--- src/features/queryEditor/layouts/queryOptions/QueryOptions.tsx
+++ src/features/queryEditor/layouts/queryOptions/QueryOptions.tsx
@@ -1,9 +1,9 @@
 import { useState } from 'react';
 import { Variables } from './variables/Variables';
 import { MdKeyboardArrowDown, MdKeyboardArrowUp } from 'react-icons/md';
-import './index.css';
 import { OptionButton } from './optionButton/OptionButton';
 import { v1 as uuidv1 } from 'uuid';
+import React from 'react';
 
 export enum TitleOptionButton {
   variables = 'Variables',
@@ -14,7 +14,7 @@ export const QueryOptions = () => {
   const [queryOptionsIsOpen, setQueryOptionsIsOpen] = useState(false);
   const [optionIsOpen, setOptionIsOpen] = useState<TitleOptionButton>(TitleOptionButton.variables);
   const HEIGHT_HEADERS_QUERY_OPTIONS = 51;
-  const MAX_HEIGHT_CODEMIRROR = 120;
+  const MAX_HEIGHT_CODEMIRROR = 82;
   const [height, setHeight] = useState(HEIGHT_HEADERS_QUERY_OPTIONS);
   const [heightCodemirror, setHeightCodemirror] = useState(0);
 
@@ -37,12 +37,16 @@ export const QueryOptions = () => {
     }
   };
 
+  const onChange = React.useCallback((value: string) => {
+    console.log('value:', value);
+  }, []);
+
   return (
     <div
-      className="qeditor-variables flex h-1/4 w-full flex-1 flex-col rounded-md border border-secondary bg-primary-background duration-500"
+      className="qeditor-variables flex h-1/3 w-full flex-1 flex-col rounded-md border border-secondary  duration-500"
       style={{ maxHeight: `${height}px` }}
     >
-      <div className="flex items-center justify-between rounded-t-lg border-b border-secondary bg-primary-background p-2">
+      <div className="flex items-center justify-between rounded-t-lg border-b border-secondary p-2">
         <div className="flex gap-1 font-thin text-secondary">
           {[TitleOptionButton.variables, TitleOptionButton.headers].map((t) => {
             return (
@@ -67,9 +71,9 @@ export const QueryOptions = () => {
         </button>
       </div>
       {optionIsOpen === TitleOptionButton.variables ? (
-        <Variables height={heightCodemirror}></Variables>
+        <Variables height={heightCodemirror} value="25"></Variables>
       ) : (
-        <div>UKMXLJVM</div>
+        <Variables height={heightCodemirror} value="1"></Variables>
       )}
     </div>
   );
--- src/features/queryEditor/layouts/queryOptions/optionButton/OptionButton.tsx
+++ src/features/queryEditor/layouts/queryOptions/optionButton/OptionButton.tsx
@@ -10,7 +10,11 @@ export const OptionButton = (props: OptionButtonProps) => {
   const { optionIsOpen, handleShowOption, title } = props;
   return (
     <button
-      className={optionIsOpen === title ? 'btn-options btn-options_active' : 'btn-options'}
+      className={
+        optionIsOpen === title
+          ? 'rounded-md border bg-neutral-100 px-2 py-1 hover:border-secondary'
+          : 'rounded-md border px-2 py-1 hover:border-secondary'
+      }
       onClick={() => handleShowOption(title)}
     >
       {title}
